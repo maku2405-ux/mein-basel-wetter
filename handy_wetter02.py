f3c6a403f3ecb1443e695a4499bb3b32d13d757c
maku2405-ux <maku2405@gmail.com>
2026-03-15 15:55:37 +0100

    Update handy_wetter02.py

--- handy_wetter02.py
+++ handy_wetter02.py
@@ -5,7 +5,7 @@ import pytz
 from PIL import Image
 import base64
 from io import BytesIO
-baslerstab = Image.open("baslerstab")
+baslerstab = Image.open("baslerstab.jpg")
 
 
 st.set_page_config(page_title="Basler Luft & Rhein", page_icon="🌊")
